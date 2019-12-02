5607fc023409ab7a1c02b510a1da4ed41eff667e
ChristianWesselborg <33611705+ChristianWesselborg@users.noreply.github.com>
2019-12-02 17:10:28 -0500

    Add files via upload
    Changes to PCA

--- Assignment_6_ChristianWesselborg.R
+++ Assignment_6_ChristianWesselborg.R
@@ -3,74 +3,45 @@
 ####Update log####
 #11-19-19 created file - maps color change - PCA updated - linear model added - preliminary decision tree added
 #11-20-19 big day - data resorted to include all variables - maps updated and working, hurray - PCA updated - decition tree reworked - random forrest added
-
+#11-21-19 added potential improvements to the maps function including new data set with polygons - not quite working yet
+#11-22-19 updated maping fuctions
+#11-29-19 cleaned map code - PCA updated, promising result
 ####setup####
 rm(list = ls())
 cittot <- read.csv("C:/Classes/4F19/Data Analytics/Assignment 6/Combined 500 Cities Data.csv")
 
-####plots for poster####
-#try new maps function
-library(urbnmapr)
+cities <- read.csv("C:/Classes/4F19/Data Analytics/Data Sets/500_Cities__Census_Tract-level_Data__GIS_Friendly_Format___2018_release.csv", header = T)
+library(ggplot2)
 library(tidyverse)
 
-#rounding to get info
-cittot_round <- cittot
-cittot_round$long <- round(cittot$Longitude, 6)
-cittot_round$lat <- round(cittot$Latitute, 6)
-city_data <- left_join(cittot_round, counties, by = "lat")
+#converts , to blank in the population metric
+as.numeric(gsub(",", "", cities$Population2010))
+cities$Population2010 <- as.numeric(gsub(",", "", cities$Population2010))
 
-#KNN to predict fips
+cit <- cities[, c(1, 2, 3, 4, 5, 6, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63)]
 
-cittot_round <- cittot
-cittot_round$long <- cittot$Longitude
-cittot_round$lat <- cittot$Latitute
+intermediate <- read.table(text=gsub('[()]', '', cit$Geolocation), 
+                           sep=",", col.names=c('Latitute', 'Longitude'))
 
-library("class")
+cit <- cbind(cit, intermediate)
+####plots for poster####
+#try new maps function
+#potential rework
+colnames(cit)[5]
+colnames(cit)[5] <- "plctract10"
 
-ind <- sample(2, nrow(counties), replace=TRUE, prob=c(0.8, 0.2))
+frame <- data.frame(mappers@data, mappers@polygons)
 
-Train <- counties[ind == 1, ]
-Test <- counties[ind == 2, ]
+ggtract <- fortify(mappers, region = "polygons")
 
-KNNpred <- knn(train = Train[, c(1, 2)], test = Test[, c(1, 2)], cl = Train$county_fips, k = 5)
-KNNpred
+ggtract <- fortify(mappers@data, region = mappers@polygons)
+typeof(ggtract)
 
-qplot(Test$long, Test$lat)
-
-redundant <- as.factor(Test$county_fips)
-table(KNNpred[1:6], redundant[1:6])
-mean(KNNpred == redundant)
-
-KNNpred <- knn(train = counties[, c(1, 2)], test = cittot_round[, c(34, 35)], cl = counties$county_fips, k = 5)
-KNNpred
-
-cittot_round$county_fips <- KNNpred
-
-#ahhhhh I did it maybe?
-
-city_data <- left_join(cittot_round, counties, by = "county_fips") 
-
-city_data %>%
-  ggplot(aes(long.y, lat.y, group = group, fill = log10(Population2010))) +
-  geom_polygon(color = "black") +
-  coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
-  labs(fill = "population")
-
-city_data %>%
-  ggplot(aes(long.y, lat.y, group = group, fill = CANCER_CrudePrev)) +
-  geom_polygon(color = "black") +
-  geom_point(aes(long.x, lat.x))
-  labs(fill = "cancer")
-
-#I did it for real
-city_data <- left_join(cittot_round, count, by = "county_fips") 
-
-count <- get_urbn_map(map = "counties", sf = T)
+mappers@data$plctract10
+city_data <- left_join(cit, ggtract, by = "plctract10")
 
 ggplot() +
-  geom_sf(count, mapping = aes(), fill = "grey") +
-  geom_sf(city_data, mapping = aes(fill = household_data$CANCER_CrudePrev, geometry = geometry))
-  
+  geom_polygon(data = city_data, aes(x = Longitude, y = Latitute))
 #base code
 ggplot() +
   geom_sf(count, mapping = aes(fill = "grey"))
@@ -79,45 +50,90 @@ ggplot() +
   geom_polygon(data = counties, aes(long, lat, group = group), color = "white", fill = "blue") +
   geom_point(aes(x = cittot$Longitude, y = cittot$Latitute)) +
   coord_map(projection = "albers", lat0 = 39, lat1 = 45)
+#maps again - may return
 
-#fips map with combined data
-library(maps)
-citcol <- cittot_round
-citcol$coler2 <- 255 * (cittot$CANCER_CrudePrev/max(citcol$CANCER_CrudePrev))
-map("county", col = "blue", fill = T)
-points(x = cittot$Longitude, y = cittot$Latitute, col = rgb(citcol$coler2/255, citcol$coler2/255, 255/255, 1))
+library(sp)
+library(rgdal)
 
-data("county.fips")
+#attempt to combine data based off stack overflow
+#very important load data mappers first, then load the following packages in the following order, then fortify works
+library(ggplot2)
+library(rgeos)
+library(maptools)
 
-us.cities
-data("world.cities")
+mappers <- readOGR("C:/Classes/4F19/Data Analytics/Assignment 6/500Cities_Tracts_11082016/500Cities_Tracts_Clip.shp")
 
-data(countyMapEnv)
+mappers@data$id <- rownames(mappers@data)
 
-citcol <- cittot_round
+mappers.fort <- fortify(mappers, region = "id")
 
-citcol$col <- cittot$CANCER_CrudePrev/max(cittot$CANCER_CrudePrev)
-summary(citcol$col)
+library(dplyr)
 
-citcol$coler <- grey(cit$CANCER_CrudePrev/max(citcol$CANCER_CrudePrev))
+city_data <- left_join(mappers.fort, mappers@data, by = "id")
 
-citcol$coler2 <- 255 * (cittot$CANCER_CrudePrev/max(citcol$CANCER_CrudePrev))
+#plots
+ggplot() +
+  geom_polygon(data = mappers[mappers$PlaceName == "New York", ], aes(x = long, y = lat, group = group), fill = mappers[mappers$PlaceName == "New York", ]$PlcTrPop10, color = "white")
 
-map("county", fill=TRUE, col= rgb(0/255, citcol$coler2/255, 255/255, 1))
+ggplot() +
+  geom_polygon(data = mappers[mappers$PlaceName == "New York", ], aes(x = long, y = lat, group = group, fill = mappers[mappers$PlaceName == "New York", ]$PlcTrPop10||NA), color = "white")
 
-citcol$coler2 <- 255 * citcol$Population2010/max(citcol$Population2010)
+ggplot() +
+  geom_polygon(data = city_data[city_data$PlaceName == "New York", ], aes(x = long, y = lat, group = group, fill = city_data[city_data$PlaceName == "New York", ]$PlcTrPop10))
 
-citcol$coler2 <- 255 * log10(cittot$Population2010)/log10(max(cittot$Population2010))
+ggplot() +
+  geom_polygon(data = city_data[city_data$PlaceName == "New York", ], aes(x = long, y = lat, group = group))
+#Now we get into it
 
-map(database = "", fill=TRUE, col= rgb(citcol$coler2/255, 0/255, 255/255, 1))
+names(cit)[names(cit) == "Place_TractID"] <- "plctract10"
 
-data("countyMapEnv")
+city_data2 <- left_join(city_data, cit, by = "plctract10")
 
-summary(cittot$Population2010)
+ggplot() +
+  geom_polygon(data = city_data2[city_data2$PlaceName.x == "New York", ], aes(x = long, y = lat, group = group, fill = city_data2[city_data2$PlaceName.x == "New York", ]$CANCER_CrudePrev))
 
-sort(cittot$Population2010, index.return = T, decreasing = T)
+ggplot() +
+  geom_polygon(data = city_data2[city_data2$PlaceName.x == "New York", ], aes(x = long, y = lat, group = group, fill = city_data2[city_data2$PlaceName.x == "New York", ]$PlcTrPop10))
 
-####PCA####
+ggplot() +
+  geom_polygon(data = city_data2[city_data2$PlaceName.x == "New York", ], aes(x = long, y = lat, group = group, fill = city_data2[city_data2$PlaceName.x == "New York", ]$CASTHMA_CrudePrev))
+
+ggplot() +
+  geom_polygon(data = city_data2, aes(x = long, y = lat, group = group, fill = city_data2$PlcTrPop10))
+
+#more maps - keep for now
+library(acs)
+api.key.install(key = "76c9a712e6a536ca5be2caa356ccfe91ea408031")
+
+#leaflet - interesting if works
+library(leaflet)
+
+map2<-leaflet() %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(data = mappers, 
+              fillColor = ~pal(PrcTrPop10), 
+              color = "#b2aeae", # you need to use hex colors
+              fillOpacity = 0.7, 
+              weight = 1, 
+              smoothFactor = 0.2,)
+map2
+
+pal <- colorNumeric(
+  palette = "YlGnBu",
+  domain = mappers$PlcTrPop10)
+
+map2<-leaflet() %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(data = mappers,
+              fillColor = ~pal(PlcTrPop10),
+              color = "#b2aeae", # you need to use hex colors
+              fillOpacity = 0.7, 
+              weight = 1, 
+              smoothFactor = 0.2,)
+map2
+
+
+  ####PCA####
 library(corpcor)
 library(GPArotation)
 library(psych)
@@ -181,6 +197,57 @@ print.psych(pc3, cut = 0.4, sort = TRUE)
 
 biplot(pc3)
 
+#PCA with county data
+library(corpcor)
+library(GPArotation)
+library(psych)
+
+cit_mat <- cor(na.omit(cit[, -c(1, 2, 3, 4, 5, 31, 35, 36, 37)]))
+cit_mat
+
+round(cit_mat, 3)
+
+det(cit_mat)
+
+pc1 <- principal(na.omit(cit[, -c(1,2,3,4,5,35,36,37)]), nfactors = 6, rotate = "none")
+pc1
+plot(pc1$values, type = "b")
+abline(1,0)
+
+pc2 <- principal(na.omit(cit[, -c(1,2,3,4,5,35,36,37)]), nfactors = 3, rotate = "varimax")
+print.psych(pc2, cut = 0.5, sort = TRUE)
+
+pc3 <- principal(na.omit(cit[, -c(1,2,3,4,5,6,35,36,37)]), nfactors = 3, rotate = "varimax")
+print.psych(pc3, cut = 0.5, sort = TRUE)
+
+pc3.1 <- principal(na.omit(cit[, -c(1,2,3,4,5,6,27,30,35,36,37)]), nfactors = 2, rotate = "varimax")
+print.psych(pc3.1, cut = 0.66, sort = TRUE)
+print.psych(pc3.1, cut = 0, sort = TRUE)
+biplot(pc3.1)
+
+#PCA with city total data
+
+pca <- principal(na.omit(cittot[, -c(1,2,32,33)]), nfactors = 6, rotate = "none")
+pca
+plot(pca$values, type = "b")
+
+pcb <- principal(na.omit(cittot[, -c(1,2,32,33)]), nfactors = 3, rotate = "varimax")
+pcb
+print.psych(pcb, cut = .5, sort = T)
+
+pcc <- principal(na.omit(cittot[, -c(1,2,32,33)]), nfactors = 2, rotate = "varimax")
+print.psych(pcc, cut = .5, sort = T)
+
+pcd <- principal(na.omit(cittot[, -c(1,2,3,6,32,33)]), nfactors = 3, rotate = "varimax")
+print.psych(pcd, cut = .5, sort = T)
+
+pce <- principal(na.omit(cittot[, -c(1,2,3,6,24,27,32,33)]), nfactors = 2, rotate = "varimax")
+print.psych(pce, cut = .5, sort = T)
+
+pce.1 <- principal(na.omit(cittot[, -c(1,2,3,6,24,27,32,33)]), nfactors = 2, rotate = "varimax")
+print.psych(pce.1, cut = .64, sort = T)
+biplot(pce.1)
+
 ####linear model####
 
 cancer_pop <- lm(cittot$CANCER_CrudePrev ~ log(cittot$Population2010, 10))
@@ -192,6 +259,19 @@ abline(cancer_pop)
 models <- lm(cittot$Population2010 ~ cittot[, -c(1,2,3)])
 summary(models)
 
+plot(cit$CANCER_CrudePrev ~ cit$ARTHRITIS_CrudePrev)
+
+plot(cit$CANCER_CrudePrev ~ cit$CASTHMA_CrudePrev)
+
+plot(cit$CANCER_CrudePrev ~ cit$TEETHLOST_CrudePrev)
+
+plot(cit$CANCER_CrudePrev ~ cit$MAMMOUSE_CrudePrev)
+
+plot(cit$CANCER_CrudePrev ~ cit$CSMOKING_CrudePrev)
+
+plot(cit$CANCER_CrudePrev ~ cit$STROKE_CrudePrev)
+
+plot(cit$CANCER_CrudePrev ~ cit$OBESITY_CrudePrev)
 ####decition tree####
 
 library(rpart)
